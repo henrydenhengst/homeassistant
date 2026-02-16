494e244a368d12ee05537e152caa93e94cade159
Henry den Hengst <henrydenhengst@gmail.com>
2026-02-16 22:06:21 +0100

    Update configuration.nix

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,76 +1,68 @@
 { config, pkgs, ... }:
 
-let
-  homelabDir = "/srv/homelab";
-in
-
 {
-  imports = [ ./hardware-configuration.nix ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  boot.loader.grub.device = "/dev/sda";
+  # ---------------------------
+  # Basis systeem
+  # ---------------------------
   networking.hostName = "homelab";
   time.timeZone = "Europe/Amsterdam";
 
+  boot.loader.grub.device = "/dev/sda";
+
+  nixpkgs.config.allowUnfree = true;
+
+  # ---------------------------
+  # User
+  # ---------------------------
   users.users.homelab = {
     isNormalUser = true;
-    extraGroups = [ "docker" "network" ];
-    password = "changeme"; # vervang door veilig wachtwoord
+    extraGroups = [ "wheel" "docker" ];
+    initialPassword = "changeme";
   };
 
-  security.wheelNeedsPassword = false;
+  security.sudo.wheelNeedsPassword = false;
 
-  # -------------------------------
-  # Docker & Compose
-  # -------------------------------
-  services.docker.enable = true;
-  virtualisation.docker-compose.enable = true;
+  # ---------------------------
+  # Docker
+  # ---------------------------
+  virtualisation.docker.enable = true;
 
-  # -------------------------------
-  # Systeem packages inclusief git/vim
-  # -------------------------------
+  # ---------------------------
+  # Essentials only
+  # ---------------------------
   environment.systemPackages = with pkgs; [
+    git
+    vim
+    curl
+    usbutils
+
+    # Firmware (voor Zigbee / BLE / WiFi dongles)
     firmware-linux
     firmware-linux-nonfree
-    firmware-misc-nonfree
     firmware-iwlwifi
     firmware-realtek
     bluez
     bluez-firmware
-    usbutils
-    curl
-    gnupg
-    lsb-release
-    ca-certificates
-    git
-    vim
   ];
 
-  nixpkgs.config.allowUnfree = true;
+  # ---------------------------
+  # SSH
+  # ---------------------------
+  services.openssh.enable = true;
 
-  # -------------------------------
-  # Maak homelab folders aan
-  # -------------------------------
+  # ---------------------------
+  # Firewall
+  # ---------------------------
+  networking.firewall.enable = true;
+
+  # ---------------------------
+  # Homelab directories
+  # ---------------------------
   systemd.tmpfiles.rules = [
     "d /srv/homelab 0755 homelab homelab -"
-    "d /srv/homelab/homeassistant 0755 homelab homelab -"
-    "d /srv/homelab/mariadb 0755 homelab homelab -"
-    "d /srv/homelab/mosquitto 0755 homelab homelab -"
-    "d /srv/homelab/node-red 0755 homelab homelab -"
-    "d /srv/homelab/influxdb 0755 homelab homelab -"
-    "d /srv/homelab/grafana 0755 homelab homelab -"
-    "d /srv/homelab/uptime-kuma 0755 homelab homelab -"
-    "d /srv/homelab/duckdns 0755 homelab homelab -"
-    "d /srv/homelab/gotify 0755 homelab homelab -"
-    "d /srv/homelab/portainer 0755 homelab homelab -"
-    "d /srv/homelab/dozzle 0755 homelab homelab -"
-    "d /srv/homelab/homepage 0755 homelab homelab -"
-    "d /srv/homelab/beszel 0755 homelab homelab -"
-    "d /srv/homelab/appdaemon 0755 homelab homelab -"
   ];
-
-  # -------------------------------
-  # Extra services
-  # -------------------------------
-  services.openssh.enable = true;
-  services.firewall.enable = true;
 }
