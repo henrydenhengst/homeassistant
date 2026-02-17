ce42cdcd532a3dd9668f0fa63bf5cebf6ede1f80
Henry den Hengst <henrydenhengst@gmail.com>
2026-02-17 07:18:18 +0100

    Create stap_0.sh

---
+++ nix/stap_0.sh
@@ -0,0 +1,8 @@
+# 1️⃣ Download het script
+wget -O disk-setup.sh https://pastebin.com/raw/dLiF0vss
+
+# 2️⃣ Maak het uitvoerbaar
+chmod +x disk-setup.sh
+
+# 3️⃣ Voer het script uit als root
+sudo ./disk-setup.sh
